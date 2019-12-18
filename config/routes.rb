0f3843cb47d8032eb78360164e32399c5e96e952
x3mprog <x3mprog@gmail.com>
2019-12-19 01:35:37 +0300

    init

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/index'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
